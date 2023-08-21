8c5420cf8264ac0ccfac72ffd04d9463d386b92a
student <student@gnu.ac.in>
2023-08-21 09:58:28 +0530

    Updated

--- app/src/main/java/com/example/mad_practical5_21012011125/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical5_21012011125/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 
@@ -46,24 +49,26 @@ class MainActivity : AppCompatActivity() {
     }
     fun toCall(no:String)
     {
-
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$no")).also{startActivity(it)}
     }
     fun openCallLog()
     {
-
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
     }
     fun openGallery()
     {
-
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
     }
-
     fun openCamera()
     {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
 
     }
     fun openAlarm()
     {
-
+        Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
     }
 
 }
+
+
